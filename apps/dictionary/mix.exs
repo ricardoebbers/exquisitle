6925f1d09245143f49a8b17a9602902f2f8475a9
Ricardo Leão <ricardo@jaya.tech>
2022-01-27 23:14:57 -0300

    chore: add code quality dependencies and alias

--- apps/dictionary/mix.exs
+++ apps/dictionary/mix.exs
@@ -11,13 +11,25 @@ defmodule Dictionary.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Dictionary.Runtime.Application, []},
       extra_applications: [:logger]
     ]
   end
@@ -25,9 +37,10 @@ defmodule Dictionary.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule ExquisitleUmbrella.MixProject do
+defmodule Exquisitle.MixProject do
   use Mix.Project
 
   def project do
@@ -6,11 +6,41 @@ defmodule ExquisitleUmbrella.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "test.watch": :test
+      ]
     ]
   end
 
   defp deps do
-    []
+    [
+      # Code coverage and test helpers
+      {:excoveralls, "~> 0.10", only: :test},
+      # Static code analysis and linter
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  def aliases do
+    [
+      quality: [
+        "format",
+        "credo --strict",
+        "dialyzer",
+        "test.watch"
+      ]
+    ]
   end
 end
