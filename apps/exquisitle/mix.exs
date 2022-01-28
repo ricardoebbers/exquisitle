05d885e6566b0e95ebc48f863851ea8c7c334b1a
Ricardo Leão <ricardo@jaya.tech>
2022-01-28 00:10:33 -0300

    feat: add exquisitle application

---
+++ apps/exquisitle/mix.exs
@@ -0,0 +1,45 @@
+defmodule Exquisitle.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :exquisitle,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      elixirc_paths: elixirc_paths(Mix.env())
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_machina, "~> 2.7.0", only: :test}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
---
+++ apps/exquisitle/test/exquisitle_test.exs
@@ -0,0 +1,3 @@
+defmodule ExquisitleTest do
+  use ExUnit.Case
+end
